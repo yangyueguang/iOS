18061959d41b875e84745b1186a4ebb965cd375d
Chao Xue 薛超 <chao.xue1.o@nio.com>
2018-12-28 23:57:38 +0800

    升级

---
+++ Freedom/utility/SwiftTools/UIView+CircularAnimation.swift
@@ -0,0 +1,150 @@
+//
+//  UIView+CircularAnimation.swift
+//  StarWarsAnimations
+//
+//  Created by Artem Sidorenko on 10/5/15.
+//  Copyright © 2015 Yalantis. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+class StarsOverlay: UIView {
+    var emitterTimer: Timer?
+    private var particle = CAEmitterCell()
+    private var emitter: CAEmitterLayer {
+        return layer as! CAEmitterLayer
+    }
+    override class var layerClass : AnyClass {
+        return CAEmitterLayer.self
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setup()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setup()
+    }
+    func setup() {
+        emitter.emitterMode = CAEmitterLayerEmitterMode.outline
+        emitter.emitterShape = CAEmitterLayerEmitterShape.circle
+        emitter.renderMode = CAEmitterLayerRenderMode.oldestFirst
+        emitter.preservesDepth = true
+        particle.contents = UIImage(named: "spark")!.cgImage
+        particle.birthRate = 10
+        particle.lifetime = 50
+        particle.lifetimeRange = 5
+        particle.velocity = 20
+        particle.velocityRange = 10
+        particle.scale = 0.02
+        particle.scaleRange = 0.1
+        particle.scaleSpeed = 0.02
+        emitter.emitterCells = [particle]
+    }
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
+        if self.window != nil {
+            if emitterTimer == nil {
+                emitterTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(StarsOverlay.randomizeEmitterPosition), userInfo: nil, repeats: true)
+            }
+        } else if emitterTimer != nil {
+            emitterTimer?.invalidate()
+            emitterTimer = nil
+        }
+    }
+    @objc func randomizeEmitterPosition() {
+        let sizeWidth = max(bounds.width, bounds.height)
+        let radius = CGFloat(arc4random()).truncatingRemainder(dividingBy: sizeWidth)
+        emitter.emitterSize = CGSize(width: radius, height: radius)
+        particle.birthRate = 10 + sqrt(Float(radius))
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        emitter.emitterPosition = self.center
+        emitter.emitterSize = self.bounds.size
+    }
+}
+
+private class CircularRevealAnimator {
+    class AnimationDelegate: NSObject, CAAnimationDelegate {
+        private let completion: () -> Void
+        init(completion: @escaping () -> Void) {
+            self.completion = completion
+        }
+        func animationDidStop(_: CAAnimation, finished: Bool) {
+            completion()
+        }
+    }
+    var completion: (() -> Void)?
+    private let layer = CALayer()
+    private let mask = CAShapeLayer()
+    let animation: CABasicAnimation = CABasicAnimation(keyPath: "path")
+    init(fillLayer: CALayer, center: CGPoint, startRadius: CGFloat, endRadius: CGFloat, color: UIColor?, invert: Bool = false) {
+        fillLayer.insertSublayer(layer, at: 0)
+        layer.backgroundColor = color?.cgColor ?? UIColor.red.cgColor
+        layer.frame = fillLayer.bounds
+        var startPath = CGPath(ellipseIn: CGRect(origin: center, size: CGSize.zero).insetBy(dx: startRadius, dy: startRadius), transform: nil)
+        var endPath = CGPath(ellipseIn: CGRect(origin: center, size: CGSize.zero).insetBy(dx: -endRadius, dy: -endRadius), transform: nil)
+        if invert {
+            var path = CGMutablePath()
+            path.addRect(layer.bounds)
+            path.addPath(startPath)
+            startPath = path
+            path = CGMutablePath()
+            path.addRect(layer.bounds)
+            path.addPath(endPath)
+            endPath = path
+        }
+        mask.path = endPath
+        mask.fillRule = CAShapeLayerFillRule.evenOdd
+        animation.fromValue = startPath
+        animation.toValue = endPath
+        animation.delegate = AnimationDelegate {
+            self.layer.mask = nil
+            self.layer.opacity = 0
+            let opacityAnimation = CABasicAnimation(keyPath: "opacity")
+            opacityAnimation.fromValue = 1
+            opacityAnimation.toValue = 0
+            opacityAnimation.duration = 0.2
+            opacityAnimation.delegate = AnimationDelegate {
+                self.layer.removeFromSuperlayer()
+                self.completion?()
+            }
+            self.layer.add(opacityAnimation, forKey: "opacity")
+            self.animation.delegate = nil
+        }
+    }
+    func start() {
+        layer.mask = mask
+        mask.frame = layer.bounds
+        mask.add(animation, forKey: "reveal")
+    }
+}
+
+extension UIView {
+    /// 从一个点扩大的动画
+    func animateCircular(withDuration duration: TimeInterval, center: CGPoint, revert: Bool = false, animations: () -> Void, completion: ((Bool) -> Void)? = nil) {
+        let snapshot = snapshotView(afterScreenUpdates: false)!
+        snapshot.frame = bounds
+        self.addSubview(snapshot)
+        let center = convert(center, to: snapshot)
+        let radius: CGFloat = {
+            let x = max(center.x, frame.width - center.x)
+            let y = max(center.y, frame.height - center.y)
+            return sqrt(x * x + y * y)
+        }()
+        var animation : CircularRevealAnimator
+        if !revert {
+            animation = CircularRevealAnimator(fillLayer: snapshot.layer, center: center, startRadius: 0, endRadius: radius, color: .red, invert: true)
+        } else {
+            animation = CircularRevealAnimator(fillLayer: snapshot.layer, center: center, startRadius: radius, endRadius: 0, color: .red)
+        }
+        animation.animation.duration = duration
+        animation.completion = {
+            snapshot.removeFromSuperview()
+            completion?(true)
+        }
+        animation.start()
+        animations()
+    }
+}
--- Freedom/utility/SwiftTools/XAlertViewController.swift
+++ Freedom/utility/SwiftTools/XAlertViewController.swift
@@ -1,58 +1,188 @@
 //
-//  NIOSOPresentViewController.swift
-//  SOKit
-//
-//  Created by Tao.liu on 2018/9/9.
-//  Copyright © 2018年 NIO. All rights reserved.
-//
-
-import Foundation
+//  XAlertViewController.swift
 import UIKit
-
-public class XAlertViewController: UIViewController, UIViewControllerTransitioningDelegate {
-    public func presentBottom(_ vc: XAlertViewController ) {
-        vc.modalPresentationStyle = .custom
-        vc.transitioningDelegate = self
-        self.present(vc, animated: true, completion: nil)
-    }
-    public func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
-        let present = PresentBottom(presentedViewController: presented, presenting: presenting)
-        return present
-    }
+enum XAlertStyle : Int {
+    case alert
+    case sheet
 }
-public class PresentBottom:UIPresentationController {
-    lazy var blackView: UIView = {
-        let view = UIView(frame: self.containerView?.bounds ?? UIScreen.main.bounds)
-        view.backgroundColor = UIColor.black.withAlphaComponent(0.3)
-        let gesture = UITapGestureRecognizer(target: self, action: #selector(hidPresent))
-        view.addGestureRecognizer(gesture)
-        return view
-    }()
-    public override init(presentedViewController: UIViewController, presenting presentingViewController: UIViewController?) {
-        super.init(presentedViewController: presentedViewController, presenting: presentingViewController)
+private class XAlertAnimation: NSObject, UIViewControllerAnimatedTransitioning {
+    var isPresenting = false
+    init(isPresenting: Bool) {
+        super.init()
+        self.isPresenting = isPresenting
     }
-
-    public override func presentationTransitionWillBegin() {
-        blackView.alpha = 0
-        containerView?.addSubview(blackView)
-        UIView.animate(withDuration: 0.5) {
-            self.blackView.alpha = 1
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.5
+    }
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        if isPresenting {
+            presentAnimationTransition(transitionContext)
+        } else {
+            dismissAnimationTransition(transitionContext)
         }
     }
-    public override func dismissalTransitionWillBegin() {
-        UIView.animate(withDuration: 0.5) {
-            self.blackView.alpha = 0
+    private func presentAnimationTransition(_ transitionContext: UIViewControllerContextTransitioning) {
+        let alertVC = transitionContext.viewController(forKey: .to) as! XAlertViewController
+        alertVC.backgroundView.alpha = 0.0
+        switch alertVC.alertStyle {
+        case .alert:
+            alertVC.alertView.alpha = 0.0
+            alertVC.alertView.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        case .sheet:
+            alertVC.alertView.transform = CGAffineTransform(translationX: 0, y: alertVC.alertView.frame.height)
+        }
+        let containerView: UIView = transitionContext.containerView
+        containerView.addSubview(alertVC.view)
+        UIView.animate(withDuration: transitionDuration(using: transitionContext), animations: {
+            alertVC.backgroundView.alpha = 1.0
+            switch alertVC.alertStyle {
+            case .alert:
+                alertVC.alertView.alpha = 1.0
+                alertVC.alertView.transform = CGAffineTransform.identity
+            case .sheet:
+                alertVC.alertView.transform = CGAffineTransform.identity
+            }
+        }) { finished in
+            transitionContext.completeTransition(true)
         }
     }
-    public override func dismissalTransitionDidEnd(_ completed: Bool) {
-        blackView.removeFromSuperview()
+    private func dismissAnimationTransition(_ transitionContext: UIViewControllerContextTransitioning) {
+        let alertVC = transitionContext.viewController(forKey: .from) as! XAlertViewController
+        UIView.animate(withDuration: transitionDuration(using: transitionContext), animations: {
+            alertVC.backgroundView.alpha = 0.0
+            switch alertVC.alertStyle {
+            case .alert:
+                alertVC.alertView.alpha = 0.0
+                alertVC.alertView.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+            case .sheet:
+                alertVC.alertView.transform = CGAffineTransform(translationX: 0, y: alertVC.alertView.frame.height)
+            }
+        }) { finished in
+            transitionContext.completeTransition(true)
+        }
     }
-    public override var frameOfPresentedViewInContainerView: CGRect {
-        return CGRect(x: 0, y: UIScreen.main.bounds.height, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-    }
-    @objc func hidPresent() {
-        self.presentedViewController.dismiss(animated: true) { }
-    }
-    
 }
 
+class XAlertViewController: UIViewController ,UIViewControllerTransitioningDelegate {
+    private lazy var tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.sigleTap(_:)))
+    private lazy var alertCenterYConstraint = NSLayoutConstraint(item: alertView, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1.0, constant: 0)
+    var alertView: UIView!
+    var alertStyle: XAlertStyle = .alert
+    var dismissCompleteClosure: (() -> Void)?
+    var backgroundView = UIView()
+    var tapBackgroundDismissEnable = true {
+        didSet {
+            tapGesture.isEnabled = tapBackgroundDismissEnable
+        }
+    }
+    required public init(withAlert alertView: UIView, preferredStyle: XAlertStyle) {
+        super.init(nibName: nil, bundle: nil)
+        self.alertView = alertView
+        self.alertStyle = preferredStyle
+        backgroundView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.4)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func loadView() {
+        super.loadView()
+        providesPresentationContextTransitionStyle = true// 是否视图控制器定义它呈现视图控制器的过渡风格
+        definesPresentationContext = true
+        modalPresentationStyle = .custom
+        transitioningDelegate = self
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if alertView == nil { return }
+        view.backgroundColor = UIColor.clear
+        view.isUserInteractionEnabled = true
+        view.insertSubview(backgroundView, at: 0)
+        view.addSubview(alertView)
+        backgroundView.addGestureRecognizer(tapGesture)
+        addAlertViewControllerConstraints()
+    }
+    // 下面设置约束
+    private func addAlertViewControllerConstraints() {
+        func addConstraint(_ item: UIView, attributes: [NSLayoutConstraint.Attribute], constant: CGFloat = 0) {
+            for attr in attributes {
+                view.addConstraint(NSLayoutConstraint(item: item, attribute: attr, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: constant))
+            }
+        }
+        func addAlertConstraint(_ attrbute: NSLayoutConstraint.Attribute, constant: CGFloat){
+            guard let leftAttribute = NSLayoutConstraint.Attribute(rawValue: 0), constant > 0 else {
+                return
+            }
+            let constraint = NSLayoutConstraint(item: alertView, attribute: attrbute, relatedBy: .equal, toItem: nil, attribute: leftAttribute, multiplier: 1, constant: constant)
+            alertView.addConstraint(constraint)
+        }
+        alertView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        addConstraint(backgroundView, attributes: [.top, .left, .right, .bottom])
+        // alert 约束为居中，如果没有宽度或宽度约束则左右各留15像素
+        if alertStyle == .alert {
+            let centerXConstraint = NSLayoutConstraint(item: alertView, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1.0, constant: 0)
+            view.addConstraint(centerXConstraint)
+            if !alertView.frame.size.equalTo(CGSize.zero) {
+                addAlertConstraint(.width, constant: alertView.frame.width)
+                addAlertConstraint(.height, constant: alertView.frame.height)
+            } else {
+                var findAlertViewWidConstraint = false
+                for constraint in alertView.constraints where constraint.firstAttribute == .width {
+                    findAlertViewWidConstraint = true
+                    break
+                }
+                if !findAlertViewWidConstraint {
+                    addAlertConstraint(.width, constant: view.frame.width - 2 * 15)
+                }
+            }
+            view.addConstraint(alertCenterYConstraint)
+            NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        }else{// sheet 约束为左右下紧贴
+            for constraint in alertView.constraints where constraint.firstAttribute == .width {
+                alertView.removeConstraint(constraint)
+                break
+            }
+            addConstraint(alertView, attributes: [.left,.right,.bottom])
+            addAlertConstraint(.height, constant: alertView.frame.height)
+        }
+        if alertView.translatesAutoresizingMaskIntoConstraints {
+            alertView.translatesAutoresizingMaskIntoConstraints = false
+        }
+        view.layoutIfNeeded()
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    @objc func sigleTap(_ tap: UITapGestureRecognizer) {
+        dismiss(animated: true)
+        dismissCompleteClosure?()
+    }
+    @objc func keyboardWillShow(_ notification: Notification) {
+        let keyboardRect: CGRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let alertViewBottomEdge = (view.frame.height - alertView.frame.height) / 2
+        let statusBarHeight = UIApplication.shared.statusBarFrame.size.height
+        let differ = keyboardRect.height - alertViewBottomEdge
+        if alertCenterYConstraint.constant == -differ - statusBarHeight { return }
+        if differ >= 0 {
+            alertCenterYConstraint.constant = -differ - statusBarHeight
+            UIView.animate(withDuration: 0.25, animations: {
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    @objc func keyboardWillHide(_ note: Notification) {
+        alertCenterYConstraint.constant = 0
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.layoutIfNeeded()
+        })
+    }
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return XAlertAnimation(isPresenting: true)
+    }
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return XAlertAnimation(isPresenting: false)
+    }
+}
