e181251484ca1041855ace6818c2a2e3faaac56d
Chao Xue 薛超 <chao.xue1.o@nio.com>
2018-12-29 21:07:35 +0800

    ...

--- Freedom/书籍收藏/书籍阅读/E_WebViewControler.h
+++ Freedom/书籍收藏/书籍阅读/E_WebViewControler.h
@@ -3,30 +3,7 @@
 //  Created by Super on 15/3/3.
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
-#import <WebKit/WebKit.h>
-@interface NJKWebViewProgressView : UIView
-@property (nonatomic) float progress;
-@property (nonatomic) UIView *progressBarView;
-@property (nonatomic) NSTimeInterval barAnimationDuration; // default 0.1
-@property (nonatomic) NSTimeInterval fadeAnimationDuration; // default 0.27
-@property (nonatomic) NSTimeInterval fadeOutDelay; // default 0.1
-- (void)setProgress:(float)progress animated:(BOOL)animated;
-@end
-extern const float NJKInitialProgressValue;
-extern const float NJKInteractiveProgressValue;
-extern const float NJKFinalProgressValue;
-typedef void (^NJKWebViewProgressBlock)(float progress);
-@protocol NJKWebViewProgressDelegate;
-@interface NJKWebViewProgress : NSObject<UIWebViewDelegate>
-@property (nonatomic, weak) id<NJKWebViewProgressDelegate>progressDelegate;
-@property (nonatomic, weak) id<UIWebViewDelegate>webViewProxyDelegate;
-@property (nonatomic, copy) NJKWebViewProgressBlock progressBlock;
-@property (nonatomic, readonly) float progress; // 0.0..1.0
-- (void)reset;
-@end
-@protocol NJKWebViewProgressDelegate <NSObject>
-- (void)webViewProgress:(NJKWebViewProgress *)webViewProgress updateProgress:(float)progress;
-@end
+#import "NJKWebViewProgressView.h"
 /*浏览器视图控制器*/
 @interface E_WebViewControler : UIViewController<UIWebViewDelegate, NJKWebViewProgressDelegate>{
     UIWebView *_webView;
