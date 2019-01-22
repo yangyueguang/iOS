2aa354ebdc42b4e69e7977175eda30613d8eaade
chao.xue1.o <chao.xue1.o@nio.com>
2019-01-22 23:38:20 +0800

    ...

---
+++ Freedom/支付宝/财富/AlipayRichViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AlipayRichViewController.swift
+//  Freedom
+//
+//  Created by Chao Xue 薛超 on 2019/1/22.
+//  Copyright © 2019 薛超. All rights reserved.
+//
+
+import UIKit
+
+class AlipayRichViewController: AlipayBaseViewController {
+
+}
